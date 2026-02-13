64d17bb39d482b45eb20c68cc23082c8d7f80a5d
ManuelSigma777 <manuelosunanava33@gmail.com>
2026-02-12 18:52:32 -0800

    El cambio en el archivo sesion_01.py

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1, 3.1416, "Manuel", mi_tupla]
 print("El primer elemento de mi lista: ", mi_lista[0])
 print("El cuarto elemento de mi lista: ", mi_lista[3])
 print("El tercer elemento de mi lista: ", mi_lista[2])
+
+#Diccionarios
+mi_diccionario = {
+    "mi_list": mi_lista,
+    "nombre": "Manuel",
+    "Pi": 3.1416,
+    "Tel": "556776556"
+}
+print("Llave para accesar a mi diccionario mi_lista ", mi_diccionario["mi_lista"])
+print("Llave para accesar a mi diccionario Pi ", mi_diccionario["Pi"])
+print("Llave para accesar a mi diccionario tel ", mi_diccionario["Tel"])
