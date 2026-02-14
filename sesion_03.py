6424d66cd4fe6f226973b4586d39581c597447ff
ManuelSigma777 <manuelosunanava33@gmail.com>
2026-02-13 16:05:42 -0800

    While loop con python

--- sesion_03.py
+++ sesion_03.py
@@ -30,3 +30,28 @@ while i < 5:
         break
 else:
     print("i es igual o mayor a 5")
+
+#Practica 2
+# Dada la lista mi_lista_2 = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+#imprimir cada elemento e la lista 3 veces y cuando sea lunes no lo incluyas
+# Usa los loop while y for
+# REsultados
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+i=0
+mi_lista_2 = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+while i < 3:
+    i += 1 
+    for d in mi_lista_2:
+            if d != "Lunes":
+                print(f"{d}!")
